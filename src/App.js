4b129601e9aa4611d9e27b1a1b949db64a6940fb
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-10-04 16:50:44 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+import Contact from './Components/Contact/Contact';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+          <Header></Header>
+          <Switch>
+              <Route exact path="/">
+                  <Home></Home>
+              </Route>
+              <Route path="/home">
+                  <Home></Home>
+              </Route>
+              <Route path="/about">
+                  <About></About>
+              </Route>
+              <Route path="/services">
+                  <Services></Services>
+              </Route>
+              <Route path="/contact">
+                  <Contact></Contact>
+              </Route>
+
+              <Route path="*">
+                  <NotFound></NotFound>
+              </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
+     const defaultStyle = {
+          color: "#fff",
+          textDecoration: "none"
+     };
+
+     const activeStyle = {
+          color: "skyblue"
+     }
+
      return (
           <Navbar bg="dark" variant="dark" expand="lg">
                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand>React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                          <Nav className="ms-auto">
-                         <Nav.Link href="#home">Home</Nav.Link>
-                         <Nav.Link href="#link">Link</Nav.Link>
+                              <Nav.Link>
+                                   <NavLink style={defaultStyle} activeStyle={activeStyle} to="/home">Home</NavLink>
+                              </Nav.Link>
+                              <Nav.Link>
+                                   <NavLink style={defaultStyle} activeStyle={activeStyle} to="/about">About Us</NavLink>
+                              </Nav.Link>
+                              <Nav.Link>
+                                   <NavLink style={defaultStyle} activeStyle={activeStyle} to="/services">Services</NavLink>
+                              </Nav.Link>
+                              <Nav.Link>
+                                   <NavLink style={defaultStyle} activeStyle={activeStyle} to="/contact">Contact</NavLink>
+                              </Nav.Link>
                          </Nav>
                     </Navbar.Collapse>
                </Container>
