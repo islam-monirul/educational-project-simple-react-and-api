84190889ee5ae855fc769ccef9732f3f7d453a71
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-10-04 16:26:13 +0600

    header design added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
     </div>
   );
 }
