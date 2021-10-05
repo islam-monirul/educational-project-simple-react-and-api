a55d94a7159828aadfa3e71bdc3fc5b548d4dbb7
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-10-05 15:01:50 +0600

    favicon modified and small design change

--- src/Components/SingleCourse/SingleCourse.js
+++ src/Components/SingleCourse/SingleCourse.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Button, Card, Col } from 'react-bootstrap';
 import { FaStar } from 'react-icons/fa';
 
 const SingleCourse = (props) => {
      // destructuring
-     const {name, details, img, rating, duration, price} = props.course;
+     const {id, name, details, img, rating, duration, price} = props.course;
      return (
           <Col>
                <Card className="h-100">
                     <Card.Img variant="top" src={img} fluid/>
                     <Card.Body>
                          <p className="text-warning"><FaStar/> {rating}</p>
-                         <h6>{name}</h6>
+                         <h6>{name} {id}</h6>
                          <p>{details}</p>
+                         <h6 className="text-dark">Duration : {duration} hrs.</h6>
                     </Card.Body>
-                    <Card.Footer className="bg-white py-3 d-flex justify-content-between">
-                         <div>
-                              <h6 className="text-dark">{duration} hrs.</h6>
-                         </div>
+                    <Card.Footer className="bg-white py-3 d-flex justify-content-between align-items-center">
                          <div>
                               <h6 className="text-danger fw-bold">BDT {price}</h6>
+                         </div>
+                         <div>
+                              <Button variant="primary">Enroll</Button>
                          </div>
                     </Card.Footer>
                </Card>
